5885e49264a19ed42a63467ef0a9f6c8d3df5318
skydream <skydream2016@gmail.com>
2021-11-12 12:19:09 +0200

    12.11

--- 1-node-farm/firstNode/index.js
+++ 1-node-farm/firstNode/index.js
@@ -1,7 +1,9 @@
+const {RSA_NO_PADDING} = require('constants');
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
-////////////////////// FILES
+///////////////////////////////// FILES
 // Blocking, Synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -9,7 +11,7 @@ const http = require('http');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File is writen!!');
 
-// Non-blocking
+//////////////////// Non-blocking, Async
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 //     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //         console.log(data2);
@@ -23,9 +25,32 @@ const http = require('http');
 // });
 // console.log('Will read file');
 
-////////////////////// SERVER
+//////////////////////////////// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    res.end('Hello server');
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is product');
+    } else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
+        });
+
+        res.end('API');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'My-own-header': 'Hello-Andrii',
+        });
+        res.end('<h1>Page not found!!!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
